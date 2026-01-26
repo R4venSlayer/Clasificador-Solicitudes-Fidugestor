e3e69dea0cd1231c288f707c9dddf9afeb6bc24d
Luis López <lucho2143103@gmail.com>
2026-01-26 14:53:48 -0500

    feat: Se agrega una funcionalidad para leer un archivo .yaml

--- backend/modulo_clasificacion/src/infrastructure/file_reader/read_file_manager.py
+++ backend/modulo_clasificacion/src/infrastructure/file_reader/read_file_manager.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from pathlib import Path
 from typing import Optional
+import yaml
 
-class FileReader():
+class FileReader:
 
-    def open_content_pandas(file_path:Path, file_extension:str, s_name:Optional[str]):
+    def open_content_pandas(self, file_path:Path, file_extension:str, s_name:Optional[str], skip_rows_number:Optional[int]):
         """
         Docstring for open_content_pandas
         
@@ -20,12 +21,32 @@ class FileReader():
         """
 
         if file_extension == "xlsx":
-            df = pd.read_excel(file_path, sheet_name = s_name)
+            df = pd.read_excel(file_path, sheet_name = s_name, skiprows=skip_rows_number) 
         
         elif file_extension == "csv":
-            df = pd.read_csv(file_path, sep=";")
+            df = pd.read_csv(file_path, sep=";", encoding="latin1")
 
         return df
+    
+    def open_yaml_content(self, file_path:Path):
+        """
+        Docstring for open_yaml_content
+        
+        Método encargado de abrir el contenido de un archivo con extensión yaml
+
+        Args:
+        -   file_path (Path) : Ruta del archivo yaml
+
+        Return:
+
+
+        """
+
+        with open(file_path, "r", encoding="utf-8") as f:
+            config = yaml.safe_load(f)
+        
+        return config
+        
     
 
 
